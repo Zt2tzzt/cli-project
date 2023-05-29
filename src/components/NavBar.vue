254adcf50bafcbfb39636ce45ab2657d554f4215
ZeT1an <584633819@qq.com>
2023-05-29 22:43:34 +0800

    starting the project;

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('哈哈哈')
+</script>
+
+<template>
+  <div class="navbar">
+    <h1>NavBar: {{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  color: red;
+}
+</style>
---
+++ src/views/Haha.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('哈哈哈')
+</script>
+
+<template>
+  <div class="haha">
+    <h1>NavBar: {{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.haha {
+  color: red;
+}
+</style>
